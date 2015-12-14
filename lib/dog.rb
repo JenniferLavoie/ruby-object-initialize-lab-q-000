e38f87e181eba22f6cebd9b2212a0bec311323da
JenniferLavoie <pele3313@gmail.com>
2015-12-14 11:54:56 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(dogs_breed)
+    @breed = dogs+breed
+  end
+
+  def initialize(name, breed = "Mut")
+    @name=name
+    @breed=breed
+  end
+
+
+
+  
+
+end
+
+
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name 
+    @name
+  end
+
+  def initialize(name)
+    @name = name
+  end
+end
